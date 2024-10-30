cf3c74f801d4f1cd4a82a0a914c92c571c6104aa
awvalent11 <awvalent11@gmail.com>
2024-10-30 15:45:55 -0500

    Adds 2 tests to GameControllerTest file

--- src/test/java/com/example/GameService/GameController/GameControllerTest.java
+++ src/test/java/com/example/GameService/GameController/GameControllerTest.java
@@ -1,19 +1,74 @@
 package com.example.GameService.GameController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class GameControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private GameController gameController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void getsAllBets(){
+    public void getNFLWeek(){
+        long daysDifference = ChronoUnit.DAYS.between(LocalDate.of(2024, 8, 27), LocalDate.now());
+
+        int expectedDays = (int) daysDifference;
+        Integer expectedWeeks = expectedDays / 7;
+        Integer intToTest = gameController.getNFLWeek();
+        assertNotNull(intToTest);
+        assertEquals(expectedWeeks, intToTest);
+    }
+
+    @Test
+    void getNFLSchedule() throws Exception {
+
+        MvcResult result;
+        result = mockMvc.perform(MockMvcRequestBuilders.
+                        get("/gambling-api/nfl-schedule")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+
+        List resultList = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
+
+        assertEquals(15, resultList.size());
 
 
 
     }
+
+
 
 }
--- src/test/java/com/example/GameService/GameController/GameServiceTest.java
+++ src/test/java/com/example/GameService/GameController/GameServiceTest.java
@@ -15,17 +15,85 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class GameServiceTest {
-//
+
 //    @Autowired
 //    private GameService gameService;
 //
 //    @Mock
 //    private GameController gameController;
 //
+//
+//    GameDTO gameDTO = new GameDTO(
+//        "ScoreId": 18776,
+//            "Season": 2024,
+//            "SeasonType": 1,
+//            "Week": 8,
+//            "Day": "2024-10-28T00:00:00",
+//            "DateTime": "2024-10-28T20:15:00",
+//            "Status": "Scheduled",
+//            "AwayTeamId": 23,
+//            "HomeTeamId": 28,
+//            "AwayTeamName": "NYG",
+//            "HomeTeamName": "PIT",
+//            "GlobalGameId": 18776,
+//            "GlobalAwayTeamId": 23,
+//            "GlobalHomeTeamId": 28,
+//            "HomeTeamScore": null,
+//            "AwayTeamScore": null,
+//            "TotalScore": 0,
+//            "HomeRotationNumber": 417,
+//            "AwayRotationNumber": 416,
+//            "GameId": 18776,
+//            "PregameOdds": [
+//        {
+//            "GameOddId": 5563687,
+//                "Sportsbook": "Scrambled",
+//                "ScoreId": 18776,
+//                "Created": "2024-10-28T14:07:09",
+//                "Updated": "2024-10-28T15:54:55",
+//                "HomeMoneyLine": -386,
+//                "AwayMoneyLine": 314,
+//                "DrawMoneyLine": null,
+//                "HomePointSpread": -8.6,
+//                "AwayPointSpread": 12.3,
+//                "HomePointSpreadPayout": -160,
+//                "AwayPointSpreadPayout": -154,
+//                "OverUnder": 52.2,
+//                "OverPayout": -154,
+//                "UnderPayout": -160,
+//                "SportsbookId": 7,
+//                "OddType": "pregame",
+//                "SportsbookUrl": "https://sportsbook.draftkings.com/event/30568623",
+//                "GameId": 18776,
+//                "Unlisted": null
+//        },
+//        {
+//            "GameOddId": 5564128,
+//                "Sportsbook": "Scrambled",
+//                "ScoreId": 18776,
+//                "Created": "2024-10-28T14:39:14",
+//                "Updated": "2024-10-28T15:54:55",
+//                "HomeMoneyLine": -393,
+//                "AwayMoneyLine": 322,
+//                "DrawMoneyLine": null,
+//                "HomePointSpread": -8.6,
+//                "AwayPointSpread": 12.3,
+//                "HomePointSpreadPayout": -157,
+//                "AwayPointSpreadPayout": -157,
+//                "OverUnder": 52.2,
+//                "OverPayout": -160,
+//                "UnderPayout": -154,
+//                "SportsbookId": 8,
+//                "OddType": "pregame",
+//                "SportsbookUrl": "https://sportsbook.fanduel.com/football/nfl/new-york-giants-@-pittsburgh-steelers-33691832",
+//                "GameId": 18776,
+//                "Unlisted": null
+//        });
+//
 //    @Test
 //    @Transactional
 //    void testconvertGames(GameDTO gameDTO){
-
+//
 //        LinkedHashMap odds = new LinkedHashMap();
 //        odds.put("Odds", );
 //
@@ -69,6 +137,6 @@ class GameServiceTest {
 //        assertEquals(expectedGame.getHome(), actualGame.getHome());
 //        assertEquals(expectedGame.getOdds(), actualGame.getOdds());
 //        assertNull(actualGame.getPosition());
-        //assertEquals(expectedGame.getExpiration(), actualGame.getExpiration());
+//        assertEquals(expectedGame.getExpiration(), actualGame.getExpiration());
 //    }
 }
