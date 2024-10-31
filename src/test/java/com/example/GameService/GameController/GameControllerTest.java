5092807534916917c9249eb57aad2fa4f19a7aae
awvalent11 <awvalent11@gmail.com>
2024-10-31 10:26:20 -0500

    Adds test for /gambling-api/nfl-week-games endpoint

--- src/test/java/com/example/GameService/GameController/GameControllerTest.java
+++ src/test/java/com/example/GameService/GameController/GameControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,9 +22,9 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -39,6 +40,80 @@ class GameControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    public static boolean isInClosedRange(Integer number) {
+        return (14 <= number && number <= 16);
+    }
+
+    public static boolean isInClosedNFL(String nflName) {
+        switch (nflName){
+
+            case "KC":
+                return true;
+            case "DEN":
+                return true;
+            case "NO":
+                return true;
+            case "WAS":
+                return true;
+            case "PHI":
+                return true;
+            case "PIT":
+                return true;
+            case "NYJ":
+                return true;
+            case "NYG":
+                return true;
+            case "SEA":
+                return true;
+            case "LV":
+                return true;
+            case "DAL":
+                return true;
+            case "ATL":
+                return true;
+            case "LAR":
+                return true;
+            case "LAC":
+                return true;
+            case "GB":
+                return true;
+            case "DET":
+                return true;
+            case "MIN":
+                return true;
+            case "TB":
+                return true;
+            case "JAX":
+                return true;
+            case "ARI":
+                return true;
+            case "CHI":
+                return true;
+            case "BAL":
+                return true;
+            case "BUF":
+                return true;
+            case "MIA":
+                return true;
+            case "CAR":
+                return true;
+            case "CIN":
+                return true;
+            case "CLE":
+                return true;
+            case "IND":
+                return true;
+            case "TEN":
+                return true;
+            case "NE":
+                return true;
+            case "HOU":
+                return true;
+            default:
+                return false;
+        }
+    }
 
     @Test
     public void getNFLWeek(){
@@ -64,6 +139,28 @@ class GameControllerTest {
         List resultList = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
 
         assertEquals(15, resultList.size());
+
+    }
+
+    @Test
+    void getNFLTodayGames() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/gambling-api/nfl-week-games"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].size()").value(6))
+                .andExpect(jsonPath("$[0]").isNumber())
+                .andExpect(jsonPath("$[3]").hasJsonPath());
+
+
+
+        MvcResult result;
+        result = mockMvc.perform(MockMvcRequestBuilders.
+                        get("/gambling-api/nfl-week-games")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+
+        List resultList = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
+        assertTrue(isInClosedRange(resultList.size()));
 
 
 
